d9cff335a137d179a96a2032ba4ad06c41ba4bb5
MarKPauL <markpaulpangan099@gmail.com>
2023-04-25 01:52:19 +0800

    added command to edit embed

---
+++ commands/admin/editembed.js
@@ -0,0 +1,50 @@
+const { SlashCommandBuilder, PermissionFlagsBits, ChannelType, EmbedBuilder } = require("discord.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("editembed")
+		.setDescription("Edit an already sent embed")
+		.addChannelOption(option =>
+			option
+				.setName("channel")
+				.setDescription("The channel where the embed message is located.")
+				.setRequired(true)
+				.addChannelTypes(ChannelType.GuildText))
+		.addStringOption(option =>
+			option
+				.setName("message")
+				.setDescription("The ID of the embed message.")
+				.setRequired(true))
+		.addStringOption(option =>
+			option
+				.setName("title")
+				.setDescription("Embed Title"))
+		.addStringOption(option =>
+			option
+				.setName("color")
+				.setDescription("Embed color (hex)"))
+		.addStringOption(option =>
+			option
+				.setName("description")
+				.setDescription("Text inside the embed"))
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
+	async execute(interaction) {
+		const ChannelID = interaction.options.getChannel("channel");
+		const messageID = interaction.options.getString("message");
+		const title = interaction.options.getString("title");
+		const description = interaction.options.getString("description").replace(/\\n/g, "\r\n");
+		const color = interaction.options.getString("color");
+
+		const channel = await interaction.guild.channels.fetch(ChannelID.id);
+		const message = await channel.messages.fetch(messageID);
+
+		const embed = EmbedBuilder.from(message.embeds[0])
+			.setTitle(title)
+			.setColor(color)
+			.setDescription(description);
+
+		message.edit({ embeds: [embed] });
+		await interaction.reply("Edited the embed.");
+		setTimeout(() => interaction.deleteReply(), 5000);
+	},
+};
