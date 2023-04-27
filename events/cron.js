3b45518457949077fcc10618f84744e21842098c
MarKPauL <markpaulpangan099@gmail.com>
2023-04-27 21:22:35 +0800

    fixed cron

--- events/cron.js
+++ events/cron.js
@@ -10,15 +10,16 @@ module.exports = {
 
 		console.log("Cron has started!!!");
 		const channel = await client.channels.fetch(ANNOUNCEMENT_CH);
-		const twntymins = Math.floor(Date.now() / 1000) + 1200;
 
 		const GW = new CronJob("10 21 * * 4", () => {
+			const twntymins = Math.floor(Date.now() / 1000) + 1200;
 			channel.send(`@everyone Guild Wars will start <t:${twntymins}:R>`);
 		});
 
 		GW.start();
 
 		const CF = new CronJob("40 20 * * 6", () => {
+			const twntymins = Math.floor(Date.now() / 1000) + 1200;
 			channel.send(`@everyone Capture Flag will start <t:${twntymins}:R>`);
 		});
 
