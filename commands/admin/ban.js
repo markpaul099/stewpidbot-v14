8aa866e3097ef2baae49b4620527ef61fd51ee1f
MarKPauL <markpaulpangan099@gmail.com>
2023-09-30 02:23:42 +0800

    fixed ban object and added logs

--- commands/admin/ban.js
+++ commands/admin/ban.js
@@ -17,26 +17,33 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 	async execute(interaction) {
 		const user = interaction.options.getUser("user");
-		const reason = interaction.options.getString("reason") || "No reason provided";
+		const reasons = interaction.options.getString("reason") || "No reason provided";
 
 		const member = await interaction.guild.members.fetch(user.id);
-		console.log(member);
+
+		const logs = await member.guild.channels.cache.find(channel => channel.name === "logs");
 
 		const errEmbed = new EmbedBuilder()
 			.setDescription(`You can't take action on ${member.displayName} since they have a higher or similar role.`)
-			.setColor("#2B65EC");
+			.setColor("#152739");
 
 		if (member.roles.highest.position >= interaction.member.roles.highest.position) {
-			return interaction.reply({ embeds: [errEmbed] });
+			await interaction.reply({ embeds: [errEmbed] });
+			return;
 		}
 
-		await member.ban(reason);
+		await member.ban({ reason: `${reasons}` });
+
 
 		const embed = new EmbedBuilder()
-			.setDescription(`Succesfully banned ${member.displayName} with reason:\n ${reason}\nUser ID: ${member.id}`)
-			.setColor("#2B65EC")
+			.setDescription(`Succesfully banned ${member.displayName} with reason:\n ${reasons}\nUser ID: ${member.id}`)
+			.setColor("#152739")
 			.setTimestamp();
 
 		await interaction.reply({ embeds: [embed] });
+		setTimeout(async () => {
+			await interaction.deleteReply();
+		}, 10000);
+		await logs.send({ embeds: [embed] });
 	},
 };
