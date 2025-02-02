867c18abe2362f04984cd1095de59914c43d9018
MarKPauL <markpaulpangan099@gmail.com>
2025-02-03 01:10:34 +0800

    remove music command

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,5 @@
-const { Events, ActivityType, EmbedBuilder } = require("discord.js");
+const { Events, ActivityType } = require("discord.js");
 const Levels = require("@markpaul099/discord-xp");
-const { Player } = require("discord-player");
 
 module.exports = {
 	name: Events.ClientReady,
@@ -34,37 +33,6 @@ ${client.channels.cache.size} channels and ${client.users.cache.size} users cach
 
 		// Set Mongo URL
 		Levels.setURL(process.env.mongoUrl);
-
-		/*
-		// Set Player
-		const player = new Player(client, {
-			ytdlOptions: {
-				quality: "highestaudio",
-				highWaterMark: 1 << 25,
-			},
-			autoRegisterExtractor: false,
-		});
-
-		await player.extractors.loadDefault();
-
-		player.events.on("playerStart", (queue, track) => {
-			const embed = new EmbedBuilder()
-				.setColor("#152739")
-				.setAuthor({ name: `${client.user.username}`, iconURL: client.user.displayAvatarURL({ dynamic: true, size: 2048, extension: "png" }) })
-				.setTitle("Now Playing")
-				.setDescription(`**[${track.title}](${track.url})**\nRequested by: ${track.requestedBy}\nDuration: ${track.duration}`)
-				.setThumbnail(`${track.thumbnail}`);
-			queue.metadata.channel.send({ embeds: [embed] });
-		});
-
-		player.events.on("playerSkip", (queue, track) => {
-			queue.metadata.channel.send(`Skipping **${track.title}** due to an issue`);
-		});
-
-		player.events.on("emptyChannel", queue => {
-			queue.metadata.channel.send("Voice channel is empty, disconnecting...");
-		});
-		*/
 
 	},
 };
